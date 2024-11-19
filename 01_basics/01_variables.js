3f2d43fa0b3de2e7c2dcf99844e01f534838ef97
Aadrika Srivastava <121657643+AadrikaSrivastava02@users.noreply.github.com>
2024-11-19 16:44:05 +0000

    2nd commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+//const value can not be changed
+const accountId=144553
+let accountEmail="aadrika@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+// accountId=14532 //not allowed 
+console.log(accountId);
