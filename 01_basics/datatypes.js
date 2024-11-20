3a724fb4bd77879c6c53b5bbb15d78512896faaf
Aadrika Srivastava <121657643+AadrikaSrivastava02@users.noreply.github.com>
2024-11-20 22:25:37 +0530

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,16 @@
+//primitive
+//7 types: String , Number, Boolean, null, undefined, Symbol, Bigint
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id === anotherId)
+
+//Reference(NoN PRIMITIVE)
+//Arrays, Objects, Functions
